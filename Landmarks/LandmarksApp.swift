a0e952fb691467d39d4fe990f3537b96b40527fd
Hidemune Takahashi <h1d3mun3.74k4h45h1@gmail.com>
2023-05-30 20:00:28 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by hidemune on 5/30/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
